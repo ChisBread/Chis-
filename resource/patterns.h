f1b2942d6b2efbf72a483376019313d15f846c2b
chisbread@gmail.com <chisbread@gmail.com>
2019-08-29 22:20:44 +0800

    patterns test

--- resource/patterns.h
+++ resource/patterns.h
@@ -1,11 +1,15 @@
 #pragma once
 #include "utils/types.h"
 namespace chis {
+//原始棋型 1~4位为值位，5~30位为棋型位。wind23棋型表转换规则
+//re.match(r'XX_*(X[O_]*X)_*XX', col[-1]).group(1) then X->11 O->10 _->00
 extern const uint32_t patterns[2724];
+
+//目标棋型 1~22位为棋型位 11为边界 00为空 10为黑子 01为白子
+//值： 1~4位为黑棋类型, 5~8为白棋类型
 class GomokuPatterns {
    public:
     GomokuPatterns() {
-        pattern_map = vector_type<uint8_t>(1 << 24, 0);
         build();
     }
     static set_type<uint32_t> &arrangement_padding(uint32_t size) {
@@ -16,17 +20,36 @@ class GomokuPatterns {
         }
         set_type<uint32_t> ret;
         for (uint32_t a : arrangement_padding(size - 1)) {
-            //存在11
-            if ((a & 0x3U) == 0x3U) {
-                ret.insert((a << 2) | 0x3U);
-            } else {
-                ret.insert((a << 2) | 0x2U);
-                ret.insert((a << 2) | 0x1U);
-                ret.insert((a << 2) | 0x0U);
-            }
+            //存在11            
+            ret.insert((a << 2) | 0x3U);
+            ret.insert((a << 2) | 0x2U);
+            ret.insert((a << 2) | 0x1U);
+            ret.insert((a << 2) | 0x0U);
         }
         genres[size] = std::move(ret);
         return genres[size];
+    }
+    //将10 01互换
+    static uint32_t reverse_pattern(uint32_t pat) {
+        uint32_t reverse =
+            (pat ^ (0x003FFFFFU));  // 11->00 10->01 01->10 00->11
+        for (uint32_t i = 0; i <= 20; i += 2) {
+            //如果对应的位为11或00
+            if (((reverse >> i) & 0x3U) == 0x3U ||
+                ((reverse >> i) & 0x3U) == 0x0U) {
+                reverse ^= (0x3U << i);  //翻转
+            }
+        }
+        return reverse;
+    }
+    //22bit中心对称翻转
+    static uint32_t reverse_22bit(uint32_t v) {
+        v = ((v >> 1)  & 0x55555555U) | ((v << 1)  & 0xAAAAAAAAU);
+        v = ((v >> 2)  & 0x33333333U) | ((v << 2)  & 0xCCCCCCCCU);
+        v = ((v >> 4)  & 0x0F0F0F0FU) | ((v << 4)  & 0xF0F0F0F0U);
+        v = ((v >> 8)  & 0x00FF00FFU) | ((v << 8)  & 0xFF00FF00U);
+        v = ((v >> 16) & 0x0000FFFFU) | ((v << 16) & 0xFFFF0000U);
+        return (v>>10);
     }
     static vector_type<uint32_t> create_more(uint32_t pat) {
         //计算需要填充的位数
@@ -65,20 +88,10 @@ class GomokuPatterns {
                     } else if (rightlen != 0) {
                         continue;
                     }
-                    //翻转黑白，使得黑白棋型一致
-                    //同时给第23位置1
-                    uint32_t reverse =
-                        (ret_pat ^ (0x003FFFFFU)) |
-                        0x00400000U;  // 11->00 10->01 01->10 00->11
-                    for (uint32_t i = 0; i <= 20; i += 2) {
-                        //如果对应的位为11或00
-                        if (((reverse >> i) & 0x3U) == 0x3U ||
-                            ((reverse >> i) & 0x3U) == 0x0U) {
-                            reverse ^= (0x3U << i);  //翻转
-                        }
-                    }
+                    
                     ret.push_back(ret_pat);
-                    ret.push_back(reverse);
+                    //翻转黑白，使得黑白棋型一致
+                    ret.push_back(reverse_pattern(ret_pat)^0x00400000U);//同时给第23位置1
                 }
             }
         }
@@ -91,16 +104,20 @@ class GomokuPatterns {
             uint8_t val = pat & 0xF;
             //去掉2位边界和4位值
             pat = pat >> 6;
-            //填充
-
-            auto more_patterns = create_more(pat);
+            auto more_patterns = create_more(pat);//填充
             for (uint32_t p : more_patterns) {
-                pattern_map[p] = val;
+                if((p >> 22) == 0) {
+                    pattern_map[p] &= 0xF0U;//去掉低四位
+                    pattern_map[p] |= val;//低四位赋值
+                } else {
+                    p &= 0x003FFFFFU;
+                    pattern_map[p] &= 0x0FU;//去掉高四位
+                    pattern_map[p] |= (val<<4);//高四位赋值
+                }
             }
         }
     }
-
    public:
-    vector_type<uint8_t> pattern_map;
+    uint8_t pattern_map[1U << 22] = {};
 };
 }  // namespace chis
--- test/board_test.cpp
+++ test/board_test.cpp
@@ -2,6 +2,7 @@
 #include "board/gomoku.hpp"
 #include "gtest/gtest.h"
 #include "resource/patterns.h"
+#include <map>
 using namespace std;
 const static chis::bsize_t SIZE = 15;
 const static chis::bsize_t OFFSET = 5;
@@ -74,8 +75,14 @@ TEST(TESTBoard, Board) {
     }
 }
 TEST(TESTBoard, Patterns) {
-    chis::GomokuPatterns pats;
-    chis::vector_type<uint8_t> pattern_map(1 << 24, 0);
+    uint8_t pattern_map[1U << 22] = {};
+    //GomokuPatterns基础操作测试
+    //reverse_pattern
+    //reverse_bit
+    //arrangement_padding
+    //create_more
+
+    //初始化测试
     for (size_t i = 0; i < 2724; ++i) {
         uint32_t pat = chis::patterns[i];
         //取值 后4位
@@ -85,13 +92,92 @@ TEST(TESTBoard, Patterns) {
         //填充
         auto more_patterns = chis::GomokuPatterns::create_more(pat);
         for (uint32_t p : more_patterns) {
-            //覆盖值一致性
-            if (pattern_map[p] != 0) {
-                EXPECT_EQ(pattern_map[p], val);
+            if((p >> 22) == 0) {
+                //值合法性
+                ASSERT_LT(p, (1U << 22));
+                //覆盖值一致性
+                if( (pattern_map[p]&0x0F) != 0) {
+                    EXPECT_EQ((pattern_map[p]&0x0F), val);
+                }
+                pattern_map[p] &= 0xF0U;//去掉低四位
+                pattern_map[p] |= val;//低四位赋值
+            } else {
+                p &= 0x003FFFFFU;
+                //值合法性
+                ASSERT_LT(p, (1U << 22));
+                //覆盖值一致性
+                if( (pattern_map[p] >> 4) != 0) {
+                    EXPECT_EQ((pattern_map[p] >> 4), val);
+                }
+                pattern_map[p] &= 0x0FU;//去掉高四位
+                pattern_map[p] |= (val<<4);//高四位赋值
             }
-            pattern_map[p] = val;
         }
     }
-    //可用性校验
-    //构造30~60个常见棋型以及其变种校验棋型表可用性
+    auto rev_pattern_test = [&](uint32_t i) {
+        uint8_t valblk = pattern_map[i]&0x0F;
+        uint8_t valwht = (pattern_map[i]>>4);
+        {//置反一致性校验 黑子白子互换结果是否一致 a.黑白颠倒 b.棋型一致
+            uint32_t ni = chis::GomokuPatterns::reverse_pattern(i);
+            uint8_t nvalblk = pattern_map[ni]&0x0F;
+            uint8_t nvalwht = (pattern_map[ni]>>4);
+            EXPECT_EQ(valblk, nvalwht);
+            EXPECT_EQ(valwht, nvalblk);
+        }
+        {//对称一致性校验 中心对称翻转结果是否一致 a.黑白颠倒 b.棋型一致
+            uint32_t ri = chis::GomokuPatterns::reverse_22bit(i);
+            uint8_t rvalblk = pattern_map[ri]&0x0F;
+            uint8_t rvalwht = (pattern_map[ri]>>4);
+            EXPECT_EQ(valblk, rvalwht);
+            EXPECT_EQ(valwht, rvalblk);
+        }
+        {//置反+对称 a.黑白不颠倒 b.棋型一致
+            uint32_t nri = chis::GomokuPatterns::reverse_22bit(chis::GomokuPatterns::reverse_pattern(i));
+            uint8_t nrvalblk = pattern_map[nri]&0x0F;
+            uint8_t nrvalwht = (pattern_map[nri]>>4);
+            EXPECT_EQ(valblk, nrvalblk);
+            EXPECT_EQ(valwht, nrvalwht);
+        }
+    };
+    //棋型属性测试 1.各种对称性
+    for(uint32_t i = 0; i < (1U << 22); ++i) {
+        rev_pattern_test(i);
+    }
+    //棋型可用性测试:构造30~60个常见棋型以及其变种,校验棋型表可用性
+    map<uint32_t, uint8_t> real_patterns = {
+        //|死棋 |0 |____XOX____
+        {0b1111111111101111111111U, 0},
+        //|眠一 |1 |____XO____X
+        {0b1111111111100000000011U, 1},//测试基础棋型是否正确
+        {0b1111111101100000000011U, 1},//测试生成是否全面
+        {0b1111111001100000000001U, 1},//测试生成是否全面
+        //|活一 |2 |___X_O____X
+        {0b1111111100100000000011U, 2},
+        //|眠二 |3 |____XO___OX
+        {0b1111111111100000001011U, 3},
+        //|活二A|4 |___X_O__O_X
+        {0b1111111100100000100011U, 4},
+        //|活二B|5 |__X__O_O__X
+        {0b1111110000100010000011U, 5},
+        //|活二C|6 |_X___OO___X
+        {0b1111000000101000000011U, 6},
+        //|眠三 |7 |____XO__OOX
+        {0b1111111111100000101011U, 7},
+        //|活三A|8 |___X_O_OO_X
+        {0b1111111100100010100011U, 8},
+        //|活三B|9 |__X__OOO__X
+        {0b1111110000101010000011U, 9},
+        //|眠四 |10|____XO_OOOX
+        {0b1111111111100010101011U, 10},
+        //|活四A|11|__XO_OOO_OX
+        {0b1111111000101010001011U, 11},
+        //|活四B|12|___X_OOOO_X
+        {0b1111111100101010100011U, 12},
+        //|成五 |13|____XOOOOOX
+        {0b1111111111101010101011U, 13},
+   };
+   for(auto &item:real_patterns) {
+    EXPECT_EQ(item.second, pattern_map[item.first]);
+    rev_pattern_test(item.first);
+   }
 }
